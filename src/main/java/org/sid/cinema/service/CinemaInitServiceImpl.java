f4323bfec21ca378b37f45e27030ce2ef54375e6
Ibrahim Diarra <33433305+diarrai@users.noreply.github.com>
2021-12-24 01:07:46 +0000

    Add files via upload

--- src/main/java/org/sid/cinema/service/CinemaInitServiceImpl.java
+++ src/main/java/org/sid/cinema/service/CinemaInitServiceImpl.java
@@ -2,7 +2,15 @@ package org.sid.cinema.service;
 
 
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
+
+import javax.transaction.Transactional;
 
 import org.sid.cinema.dao.CategorieRepository;
 import org.sid.cinema.dao.CinemaRepository;
@@ -11,14 +19,23 @@ import org.sid.cinema.dao.PlaceRepository;
 import org.sid.cinema.dao.ProjectionRepository;
 import org.sid.cinema.dao.SalleRepository;
 import org.sid.cinema.dao.SeanceRepository;
+import org.sid.cinema.dao.TicketRepository;
 import org.sid.cinema.dao.VilleRepository;
+import org.sid.cinema.entities.Categorie;
 import org.sid.cinema.entities.Cinema;
+import org.sid.cinema.entities.Film;
+import org.sid.cinema.entities.Place;
+import org.sid.cinema.entities.Projection;
 import org.sid.cinema.entities.Salle;
+import org.sid.cinema.entities.Seance;
+import org.sid.cinema.entities.Ticket;
 import org.sid.cinema.entities.Ville;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@Transactional
 public class CinemaInitServiceImpl implements ICinemaInitService {
     @Autowired
 	private VilleRepository villeRepository;
@@ -36,7 +53,8 @@ public class CinemaInitServiceImpl implements ICinemaInitService {
     private ProjectionRepository projectionRepository;
     @Autowired
     private CategorieRepository categorieRepository;
-	
+	@Autowired
+    private TicketRepository ticketRepository;
 	@Override
 	public void initVilles() {
    Stream.of("Bamako", "Koulikoro", "Sikasso", "Segou", "Kayes", "Mopti", "Gao", "Tombouctou", "Kidal", "Koutiala").forEach(nameVille->{
@@ -49,9 +67,10 @@ public class CinemaInitServiceImpl implements ICinemaInitService {
 	}
 
 	@Override
+
 	public void initCinemas() {
 		villeRepository.findAll().forEach(v->{
-			Stream.of("CineSayon", "Babemba", "IMAX", "MagicCine")
+			Stream.of("CineSayon", "Babemba", "IMAX", "MagicCine", "CineGuimbi")
 			.forEach(nameCinema->{
 				Cinema cinema=new Cinema();
 				cinema.setName(nameCinema);
@@ -81,37 +100,96 @@ public class CinemaInitServiceImpl implements ICinemaInitService {
 
 	@Override
 	public void initPlaces() {
-		// TODO Auto-generated method stub
+		salleRepository.findAll().forEach(salle->{
+			for(int i=0; i<salle.getNombrePlace(); i++) {
+				Place place = new Place();
+				place.setNumero(i+1);
+				place.setSalle(salle);
+				placeRepository.save(place);
+			}
+		});
 		
 	}
 
 	@Override
 	public void initSeances() {
-		// TODO Auto-generated method stub
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		Stream.of("12:00","15:00","18:05","20:15", "21:00").forEach(s->{
+			Seance seance = new Seance();
+			try {
+				seance.setHeureDebut(dateFormat.parse(s));
+				seanceRepository.save(seance);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 		
 	}
 
 	@Override
 	public void initCategories() {
-		// TODO Auto-generated method stub
+		Stream.of("Actions", "Drame", "Histoire", "Fiction").forEach(cat->{
+			Categorie categorie = new Categorie();
+			categorie.setName(cat);
+			categorieRepository.save(categorie);
+		});
 		
 	}
 
 	@Override
 	public void initFilms() {
-		// TODO Auto-generated method stub
+		double[] durees=new double[] {1,1.5,2,2.5,3,3.5};
+		List<Categorie> categorie = categorieRepository.findAll();
+		Stream.of("Black Swan", "Cartel", "Game of Thrones", "Le Passe", "Love Story", "Titanic").forEach(f->{
+			Film film = new Film();
+			film.setTitre(f);
+			film.setDuree(durees[new Random().nextInt(durees.length)]);
+			film.setPhoto(f.replaceAll(" ", "")+".jpg");
+			film.setCategorie(categorie.get(new Random().nextInt(categorie.size())));
+			filmRepository.save(film);
+		});
 		
 	}
 
 	@Override
 	public void initProjections() {
-		// TODO Auto-generated method stub
+		double[] prices = new double[] {5000,6000,3000,3500,2000,2500};
+		List<Film> films=filmRepository.findAll();
+		villeRepository.findAll().forEach(ville->{
+			ville.getCinemas().forEach(cinema->{
+				cinema.getSalles().forEach(salle->{
+					int index=new Random().nextInt(films.size());
+					Film film = films.get(index);
+						seanceRepository.findAll().forEach(seance->{
+						Projection projection=new Projection();
+						projection.setDateProjection(new Date());
+						projection.setFilm(film);
+						projection.setPrix(prices[new Random().nextInt(prices.length)]);
+						projection.setSalle(salle);
+						projection.setSeance(seance);
+						projectionRepository.save(projection);
+							
+						});
+					
+				});
+			});
+		});
 		
 	}
 
 	@Override
 	public void initTickets() {
-		// TODO Auto-generated method stub
+		projectionRepository.findAll().forEach(p->{
+			p.getSalle().getPlaces().forEach(place->{
+				Ticket ticket = new Ticket();
+				ticket.setPlaces(place);
+				ticket.setPrix(p.getPrix());
+				ticket.setProjections(p);
+				ticket.setReserve(false);
+				ticketRepository.save(ticket);
+			});
+		});
 		
 	}
 
